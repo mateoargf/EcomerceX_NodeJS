20886612564335cc5e8397735a154e1827fd3f53
Lucas-Sop <lgomez.sop@gmail.com>
2023-10-31 00:25:54 -0300

    Se agregaron pages: category-checkPage-login-moreFrom-wishlist-productGroup ademas se pusieron link de imagenes

--- controllers/products.js
+++ controllers/products.js
@@ -6,15 +6,31 @@ const getAllProduct = (req, res) => {
      res.status(200).json({ msg: 'probando testeando rutas DOS' })
 }
 
-const getAllCategory = (req, res) => {
-     res.status(200).render('pages/category')
+const getAllGroup = (req, res) => {
+     res.status(200).render('pages/productGroup')
 }
 const getMoreFrom = (req, res) => {
      res.status(200).render('pages/moreFrom')
 }
+const getCheckPage = (req, res) => {
+     res.status(200).render('pages/checkPage')
+}
+const getWishList = (req, res) => {
+     res.status(200).render('pages/wishlist')
+}
+const getLogin = (req, res) => {
+     res.status(200).render('pages/login')
+}
+const getCategory = (req, res) => {
+     res.status(200).render('pages/category')
+}
 module.exports = {
      getAllProduct,
      getHydro,
-     getAllCategory,
-     getMoreFrom
+     getAllGroup,
+     getMoreFrom,
+     getCheckPage,
+     getWishList,
+     getLogin,
+     getCategory
 }
--- routes/products.js
+++ routes/products.js
@@ -4,15 +4,23 @@ const router = Router()
 const {
      getAllProduct,
      getHydro,
-     getAllCategory,
-     getMoreFrom
+     getMoreFrom,
+     getCheckPage,
+     getWishList,
+     getLogin,
+     getAllGroup,
+     getCategory
 } = require('../controllers/products')
 
 // rutas
 router.get('/', getAllProduct)
 router.get('/hydro', getHydro)
-router.get('/category', getAllCategory)
+router.get('/group', getAllGroup)
 router.get('/morefrom', getMoreFrom)
+router.get('/checkPage', getCheckPage)
+router.get('/wishlist', getWishList)
+router.get('/login', getLogin)
+router.get('/category', getCategory)
 // router.get('/articulo/:id', getSelectedProduct)
 // router.post('/crear', postAllProduct)
 // router.delete('/eliminar/:id', deleteAllProduct)
