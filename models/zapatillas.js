c8ddbc90158327106f1b6239b0351fdf4a6c8449
Lucas-Sop <lgomez.sop@gmail.com>
2023-11-02 00:22:15 -0300

    Se agregaron esquemas

---
+++ models/zapatillas.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const shoeSchema = new mongoose.Schema({
+  marca: {
+    type: String,
+    required: true,
+  },
+  modelo: {
+    type: String,
+    required: true,
+  },
+  imagen: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        // Utiliza una expresión regular para verificar la extensión de la imagen
+        return /\.(jpg|jpeg|png|gif)$/.test(value.toLowerCase());
+      },
+      message: 'La URL de la imagen no es válida.',
+    },
+  },
+  talle: {
+    type: Number,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+  },
+  descripción: {
+    type: String,
+    required: true,
+  },
+  categoría: {
+    type: String,
+    required: true,
+  },
+  valoraciones: {
+    type: [
+      {
+        usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario' },
+        calificación: { type: Number, required: true },
+        comentario: { type: String },
+      },
+    ],
+  },
+});
+
+module.exports = mongoose.model('Zapatilla', shoeSchema);
