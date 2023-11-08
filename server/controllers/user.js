2c9008c24917098a8c76bb74bdc628bbbf5e09e2
Mateo Gómez Fleytas <mateogf@hotmail.com>
2023-11-08 14:51:24 -0300

    ver si en el postFormularioRegistro van como variables los atributos del modelo

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,6 +2,11 @@ const getFormularioRegistro = (req, res) => {
      res.status(200).render('pages/registro')
 }
 
+const postFormularioRegistro = async (req, res) => {
+     // const {nombres modelo?}
+}
+
 module.exports = {
-     getFormularioRegistro
+     getFormularioRegistro,
+     postFormularioRegistro
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,10 +3,14 @@ const router = Router()
 
 // asignamos las funciones de controllers a routes
 const {
-     getFormularioRegistro
+     getFormularioRegistro,
+     postFormularioRegistro
 } = require('../controllers/user')
 
-// rutas:
+// rutas: GET
 router.get('/registro', getFormularioRegistro)
+
+// rutas: POST
+router.post('/registro', postFormularioRegistro)
 
 module.exports = router
