c8186df6eca854ad8e1779b5ee70b24287cebadc
Mateo Gómez Fleytas <mateogf@hotmail.com>
2023-11-13 15:51:07 -0300

    ver por que no envía los json a la db

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,3 +1,11 @@
+// usamos propiedades de mongoose para enviar, actualizar, obtener o eliminar datos de la db
+const mongoose = require('mongoose')
+// modelos productos
+const Campera = require('../models/camperas')
+
+// jsons productos
+const camperaData = require('../jsons/camperas.json')
+
 const getHydro = (req, res) => {
      res.status(200).render('pages/hydroShoes')
 }
@@ -23,8 +31,17 @@ const getCategory = (req, res) => {
      res.status(200).render('pages/category')
 }
 
-const postAllProduct = (req, res) => {
-     res.status(200).json({msg:'crear productos'})
+// enviamos los productos a la coleccion productos
+const postAllProduct = async(req, res) => {
+     // camperas envio
+     try {
+          const resultado = await Campera.insertMany(camperaData)
+          console.log(`JSON enviado con éxito ${resultado}`)
+     } catch(error) {
+          console.log(`Error al enviar los datos ${error}`)
+     } finally {
+          mongoose.disconnect()
+     }
 }
 
 module.exports = {
