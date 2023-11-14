f7ef38d8b4e8819d60cdc39fc5f550c6b390a75f
ayrtonGonzales <ayrtongonzales-2003@outlook.com>
2023-11-14 10:02:58 -0300

    Boton carrito ya tiene funcionalidad

---
+++ views/pages/scripts/shoeCard.js
@@ -0,0 +1,12 @@
+//Archivo designado para la funcionalidad del boton "carrito" de la pagina localhost:3600/prod/hydr
+
+// document.addEventListener("DOMContentLoaded", function(){
+
+//     const agregarBtn = producto.querySelector(".agregarBtn");
+
+//     agregarBtn.addEventListener("click", function(){
+//         alert("hola");
+//     })
+
+// })
+
